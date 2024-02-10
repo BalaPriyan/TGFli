b6f113eeada17c303921690757a8b082813ee06b
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-02-10 20:20:11 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -3,17 +3,17 @@ import os
 
 class Config:
 
-    TG_APP_ID:int
-    TG_APP_HASH :str
-    SESSION_STRING :str
-    ADMIN_UID:int
+    TG_APP_ID = '28583769'
+    TG_APP_HASH = '8c03cfc6e8be855de935ac61ac4047d7'
+    SESSION_STRING = '1BJWap1sBuxfjwBGevZY_OIFwYuBsOkXs2smsyLQ4kwOMfWvjsA_8Mz2wySGf1Fsck4GbFqbQ0aK-owQuHbisz5zkcF88zBJnGvr7NIaRkvJ2zFOTFIhhui_pZ3IhhlJRSXj0CiJNhS-YELZjg6L_oJ9Azo5gdV31RhUlR2NNrKKS7jiE-zd-hMkS38saHsDj9hgk27Pj309SKA_a8-LBntYrHiy_bSa4XjIoIFv2POIBZg_xZOu2d5flaUmTL4-vGmaYqLVWT4_q6GvQo667nz8DKqX9iBz3XmjVtuczRtGOV8sgiTC8JS_bmCSZ0BISqnKF_ziNmJ7IFIzLgyDbVS2_FvKgNSc='
+    ADMIN_UID = '6675988213'
     
-    DATABASE_URL:str
+    DATABASE_URL = 'postgres://koyeb-adm:ftqdyolX9Tg0@ep-quiet-resonance-a2yei523.eu-central-1.pg.koyeb.app/koyebdb'
 
 
     APP_NAME        = 'Movie Blog'
     APP_DESC        = 'A simple telegram indexer for movie channel'
-    APP_URL         = 'https://tgflix.herokuapp.com'
+    APP_URL         = 'https://filexlink-tomenmain.koyeb.app'
 
     BUFFER_SIZE:int                 = 1024
     MAX_ITEMS_PER_PAGE:int          = 20
